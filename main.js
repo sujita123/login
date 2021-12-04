2ee9015dc52a45fd696c3516d96a6875cdcecab0
Sujita Kc <kcsuji.kc@gmail.com>
2021-12-05 07:45:05 +1100

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const Email = document.getElementById("Email")
+const Password = document.getElementById("Password")
+const form = document.getElementById("login")
+
+form.addEventListener('Login', (e) =>{
+  let messages =[]
+  if (Email, value=== ''|| Email.value== null){
+    messages.push('Email is required')
+  }
+  if 
+})
